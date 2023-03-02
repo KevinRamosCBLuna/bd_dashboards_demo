70c30393b0f5d03e6f6d1ff2ac8245043f5e95b4
Kevin Ramos <kevin.ramos@cbluna.com>
2023-03-02 10:09:01 -0800

    .

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -842,11 +842,11 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "797b3362ddc89f207f42b503e18e47b5",
-"/": "797b3362ddc89f207f42b503e18e47b5",
+"index.html": "6f674f7b9d0ca56e96acbc31b2723f07",
+"/": "6f674f7b9d0ca56e96acbc31b2723f07",
 "js/pdf.js": "f33791e232e5a3443951e49cfc21f55c",
 "js/pdf.worker.min.js": "a4fab6c4601042d77d85c2e89ef4c3e3",
-"main.dart.js": "e36bd0ded4ba6c24c6fd73c41ba8736f",
+"main.dart.js": "dddc5f507eda3ffe160446bda49dbf79",
 "manifest.json": "a7f0d160bb521192e7ed154a4752e4a9",
 "version.json": "f41613f78ce45f3bb1a6e936c52b5d16"
 };
